f0736fc0fa2281ca481ea0bf5ab428491dd3c829
Gustavo Daniel <gustavodanieldetoledo@gmail.com>
2021-08-04 20:34:43 -0300

    Add configuracion for database and migrations of database

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,13 @@ import os
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 from config import app_config
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app(config_name=os.environ.get('CONFIG_NAME', 'development')):
@@ -14,6 +17,7 @@ def create_app(config_name=os.environ.get('CONFIG_NAME', 'development')):
     app.config.from_object(app_config[config_name])
 
     db.init_app(app)
+    migrate.init_app(app, db)
     CORS(app)
 
     return app
