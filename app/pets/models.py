5e7778fb63647d747cac1a20a8446a6ff906a9da
Gustavo Daniel <gustavodanieldetoledo@gmail.com>
2021-08-05 20:50:33 -0300

    Add pet_photo in pets model

--- app/pets/models.py
+++ app/pets/models.py
@@ -10,3 +10,4 @@ class Pets(db.Model):
     pet_owner_name = db.Column(db.String(250), nullable=True)
     breed = db.Column(db.String(250), nullable=True)
     birth_date = db.Column(db.String, nullable=True)
+    pet_photo = db.Column(db.String, nullable=True)
