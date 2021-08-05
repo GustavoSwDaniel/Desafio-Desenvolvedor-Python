3a2c31a4a46a8e5ffbf7d7735f27eabd12e8dbbf
Gustavo Daniel <gustavodanieldetoledo@gmail.com>
2021-08-05 20:51:13 -0300

    Modify nullable field for all field except pet_photo field

--- app/pets/models.py
+++ app/pets/models.py
@@ -6,8 +6,8 @@ class Pets(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
 
-    name_pet = db.Column(db.String(250), nullable=True)
-    pet_owner_name = db.Column(db.String(250), nullable=True)
-    breed = db.Column(db.String(250), nullable=True)
-    birth_date = db.Column(db.String, nullable=True)
+    name_pet = db.Column(db.String(250), nullable=False)
+    pet_owner_name = db.Column(db.String(250), nullable=False)
+    breed = db.Column(db.String(250), nullable=False)
+    birth_date = db.Column(db.String, nullable=False)
     pet_photo = db.Column(db.String, nullable=True)
