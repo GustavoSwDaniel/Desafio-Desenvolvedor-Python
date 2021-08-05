765e755a44ea4b5f55c32329711700028206069e
Gustavo Daniel <gustavodanieldetoledo@gmail.com>
2021-08-05 00:54:24 -0300

    Add excepetion object does not found error

--- app/commons/exceptions.py
+++ app/commons/exceptions.py
@@ -2,3 +2,9 @@ class BasicError(Exception):
     def __init__(self, message):
         Exception.__init__(self)
         self.message = message
+
+
+class ObjectDoesNotFoundError(BasicError):
+    def __init__(self, code_status=404, message='Object does not found'):
+        super().__init__(message)
+        self.code_status = code_status
